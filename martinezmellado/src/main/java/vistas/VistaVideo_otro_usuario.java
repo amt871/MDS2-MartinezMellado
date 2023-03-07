bf93842e08dde74627ec432793e3ca28152717b1
Alejandro Martinez Terrones <amt871@inlumine.ual.es>
2023-03-07 12:52:11 +0100

    Algunas vistas creadas

---
+++ martinezmellado/src/main/java/vistas/VistaVideo_otro_usuario.java
@@ -0,0 +1,24 @@
+package vistas;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+
+/**
+ * A Designer generated component for the vista-video_otro_usuario template.
+ *
+ * Designer will add and remove fields with @Id mappings but
+ * does not overwrite or otherwise change this file.
+ */
+@Tag("vista-video_otro_usuario")
+@JsModule("./src/vista-video_otro_usuario.ts")
+public class VistaVideo_otro_usuario extends LitTemplate {
+
+    /**
+     * Creates a new VistaVideo_otro_usuario.
+     */
+    public VistaVideo_otro_usuario() {
+        // You can initialise any data required for the connected UI components here.
+    }
+
+}
---
+++ martinezmellado/src/main/java/vistas/VistaVista_detalle__administrador_.java
@@ -0,0 +1,24 @@
+package vistas;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+
+/**
+ * A Designer generated component for the vista-vista_detalle__administrador_ template.
+ *
+ * Designer will add and remove fields with @Id mappings but
+ * does not overwrite or otherwise change this file.
+ */
+@Tag("vista-vista_detalle__administrador_")
+@JsModule("./src/vista-vista_detalle__administrador_.ts")
+public class VistaVista_detalle__administrador_ extends LitTemplate {
+
+    /**
+     * Creates a new VistaVista_detalle__administrador_.
+     */
+    public VistaVista_detalle__administrador_() {
+        // You can initialise any data required for the connected UI components here.
+    }
+
+}
