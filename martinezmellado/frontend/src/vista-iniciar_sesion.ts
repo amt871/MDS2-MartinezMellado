ae4a74e931b5c0470937d4eb99b70c4a9dedbe18
Alejandro Martiínez Terrones <amt871@inlumine.ual.es>
2023-03-14 12:55:44 +0100

    Base de iniciar sesion hechas

--- martinezmellado/frontend/src/vista-iniciar_sesion.ts
+++ martinezmellado/frontend/src/vista-iniciar_sesion.ts
@@ -1,4 +1,6 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
 
 @customElement('vista-iniciar_sesion')
 export class VistaIniciar_sesion extends LitElement {
@@ -12,7 +14,15 @@ export class VistaIniciar_sesion extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vaadin-vertical-layout theme="spacing" style="height: 100%;">
+ <vaadin-horizontal-layout theme="spacing" style="height: 30%; width: 100%;"></vaadin-horizontal-layout>
+ <vaadin-horizontal-layout class="content" style="width: 100%; height: 100%;">
+  <vaadin-horizontal-layout theme="spacing" style="width: 100%;"></vaadin-horizontal-layout>
+  <vaadin-horizontal-layout theme="spacing" style="width: 100%;"></vaadin-horizontal-layout>
+ </vaadin-horizontal-layout>
+</vaadin-vertical-layout>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
--- martinezmellado/src/main/java/vistas/VistaIniciar_sesion.java
+++ martinezmellado/src/main/java/vistas/VistaIniciar_sesion.java
@@ -3,6 +3,8 @@ package vistas;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.component.template.Id;
 
 /**
  * A Designer generated component for the vista-iniciar_sesion template.
@@ -14,7 +16,10 @@ import com.vaadin.flow.component.littemplate.LitTemplate;
 @JsModule("./src/vista-iniciar_sesion.ts")
 public class VistaIniciar_sesion extends LitTemplate {
 
-    /**
+    @Id("vaadinVerticalLayout")
+	private Element vaadinVerticalLayout;
+
+	/**
      * Creates a new VistaIniciar_sesion.
      */
     public VistaIniciar_sesion() {
