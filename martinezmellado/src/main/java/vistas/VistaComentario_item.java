bcc4ff93d589414314da8a5f5335fdba89ea271c
Juan Raul Mellado Garcia <jmg061@inlumine.ual.es>
2023-03-14 11:33:36 +0100

    Gitignore

---
+++ martinezmellado/src/main/java/vistas/VistaComentario_item.java
@@ -0,0 +1,24 @@
+package vistas;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+
+/**
+ * A Designer generated component for the vista-comentario_item template.
+ *
+ * Designer will add and remove fields with @Id mappings but
+ * does not overwrite or otherwise change this file.
+ */
+@Tag("vista-comentario_item")
+@JsModule("./src/vista-comentario_item.ts")
+public class VistaComentario_item extends LitTemplate {
+
+    /**
+     * Creates a new VistaComentario_item.
+     */
+    public VistaComentario_item() {
+        // You can initialise any data required for the connected UI components here.
+    }
+
+}
---
+++ martinezmellado/src/main/java/vistas/VistaConfigurar_mi_perfil.java
@@ -0,0 +1,24 @@
+package vistas;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+
+/**
+ * A Designer generated component for the vista-configurar_mi_perfil template.
+ *
+ * Designer will add and remove fields with @Id mappings but
+ * does not overwrite or otherwise change this file.
+ */
+@Tag("vista-configurar_mi_perfil")
+@JsModule("./src/vista-configurar_mi_perfil.ts")
+public class VistaConfigurar_mi_perfil extends LitTemplate {
+
+    /**
+     * Creates a new VistaConfigurar_mi_perfil.
+     */
+    public VistaConfigurar_mi_perfil() {
+        // You can initialise any data required for the connected UI components here.
+    }
+
+}
