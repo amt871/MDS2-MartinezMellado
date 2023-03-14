8beef4fd3a6cd26022c5f420f0a33bdca5e7f463
Alejandro Martiínez Terrones <amt871@inlumine.ual.es>
2023-03-14 12:23:22 +0100

    Carcasas completas

---
+++ martinezmellado/frontend/src/vista-usuario__no_registrado__item.ts
@@ -0,0 +1,22 @@
+import { LitElement, html, css, customElement } from 'lit-element';
+
+@customElement('vista-usuario__no_registrado__item')
+export class VistaUsuario__no_registrado__item extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+          display: block;
+          height: 100%;
+      }
+      `;
+  }
+
+  render() {
+    return html``;
+  }
+
+  // Remove this method to render the contents of this view inside Shadow DOM
+  createRenderRoot() {
+    return this;
+  }
+}
