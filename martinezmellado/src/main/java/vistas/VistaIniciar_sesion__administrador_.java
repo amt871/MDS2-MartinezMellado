30f189fd34f3e6e18b4e09af76e95314184aee51
Mellado Garcia Juan Raul (jmg061) <jmg061@inlumine.ual.es>
2023-03-16 17:53:22 +0100

    IniciarSesionAdministrador

--- martinezmellado/src/main/java/vistas/VistaIniciar_sesion__administrador_.java
+++ martinezmellado/src/main/java/vistas/VistaIniciar_sesion__administrador_.java
@@ -3,6 +3,10 @@ package vistas;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.template.Id;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.html.Label;
 
 /**
  * A Designer generated component for the vista-iniciar_sesion__administrador_ template.
@@ -14,11 +18,25 @@ import com.vaadin.flow.component.littemplate.LitTemplate;
 @JsModule("./src/vista-iniciar_sesion__administrador_.ts")
 public class VistaIniciar_sesion__administrador_ extends LitTemplate {
 
-    /**
+    @Id("bIniciarSesion")
+	private Button bIniciarSesion;
+	@Id("inPass")
+	private TextField inPass;
+	@Id("inUser")
+	private TextField inUser;
+	@Id("LabelUsuario")
+	private Label labelUsuario;
+	@Id("labelNombreApp")
+	private Label labelNombreApp;
+	@Id("iconButton")
+	private Button iconButton;
+
+	/**
      * Creates a new VistaIniciar_sesion__administrador_.
      */
     public VistaIniciar_sesion__administrador_() {
         // You can initialise any data required for the connected UI components here.
+    	
     }
 
 }
