5d49d87e99db15fd671771e6b05ac93fd6353fcf
Mellado Garcia Juan Raul (jmg061) <jmg061@inlumine.ual.es>
2023-03-23 17:08:12 +0100

    mas iconos

--- martinezmellado/frontend/src/vista-denuncias.ts
+++ martinezmellado/frontend/src/vista-denuncias.ts
@@ -1,4 +1,10 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import './vista-cabecera__administrador_';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-scroller.js';
+import './vista-denuncias_item';
 
 @customElement('vista-denuncias')
 export class VistaDenuncias extends LitElement {
@@ -12,7 +18,31 @@ export class VistaDenuncias extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vaadin-vertical-layout style="width: 100%; height: 100%;">
+ <vista-cabecera__administrador_ style="width: 100%; height: 5%;"></vista-cabecera__administrador_>
+ <vaadin-horizontal-layout theme="spacing-xl" style="width: 100%; height: 5%; align-self: center; align-items: center; justify-content: center;">
+  <vaadin-button style="width: 20%;">
+   Todas
+  </vaadin-button>
+  <vaadin-button style="width: 20%;">
+   Pendientes
+  </vaadin-button>
+  <vaadin-button style="width: 20%;">
+   Vistas 
+  </vaadin-button>
+  <vaadin-button style="width: 20%;">
+   Aplicadas 
+  </vaadin-button>
+ </vaadin-horizontal-layout>
+ <vaadin-scroller style="width: 100%; height: 100%;">
+  <vista-denuncias_item style="width: 100%; height: 20%; margin: var(--lumo-space-s);"></vista-denuncias_item>
+  <vista-denuncias_item style="height: 20%; margin: var(--lumo-space-s);"></vista-denuncias_item>
+  <vista-denuncias_item style="height: 20%; margin: var(--lumo-space-s);"></vista-denuncias_item>
+  <vista-denuncias_item style="height: 20%; margin: var(--lumo-space-s);"></vista-denuncias_item>
+ </vaadin-scroller>
+</vaadin-vertical-layout>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
--- martinezmellado/frontend/src/vista-denuncias_item.ts
+++ martinezmellado/frontend/src/vista-denuncias_item.ts
@@ -1,4 +1,7 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
 @customElement('vista-denuncias_item')
 export class VistaDenuncias_item extends LitElement {
@@ -12,7 +15,27 @@ export class VistaDenuncias_item extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vaadin-horizontal-layout class="content" style="width: 100%; height: 100%;">
+ <vaadin-vertical-layout theme="spacing">
+  <img style="height: 100%; margin-left: var(--lumo-space-s);" src="../../resources/icons/java.svg">
+ </vaadin-vertical-layout>
+ <vaadin-horizontal-layout style="flex-grow: 0; justify-content: center; width: 100%; flex-shrink: 1;">
+  <label style="align-self: center;">Fecha de la denuncia</label>
+ </vaadin-horizontal-layout>
+ <vaadin-vertical-layout theme="spacing" style="align-items: center; justify-content: center; margin-right: var(--lumo-space-s); flex-grow: 0; flex-shrink: 1; width: 50%;">
+  <vaadin-button style="width: 100%;">
+   Aceptar
+  </vaadin-button>
+  <vaadin-button style="width: 100%;">
+   Rechazar
+  </vaadin-button>
+  <vaadin-button style="width: 100%;">
+   Posponer
+  </vaadin-button>
+ </vaadin-vertical-layout>
+</vaadin-horizontal-layout>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
