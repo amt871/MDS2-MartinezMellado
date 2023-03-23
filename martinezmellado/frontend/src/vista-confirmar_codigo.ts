70e0e4ac2995e66d81243619eef1975f304cc14a
Alejandro Martiínez Terrones <amt871@inlumine.ual.es>
2023-03-23 11:24:21 +0100

    Cibernauta acabado

--- martinezmellado/frontend/src/vista-confirmar_codigo.ts
+++ martinezmellado/frontend/src/vista-confirmar_codigo.ts
@@ -1,4 +1,7 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
 @customElement('vista-confirmar_codigo')
 export class VistaConfirmar_codigo extends LitElement {
@@ -12,7 +15,20 @@ export class VistaConfirmar_codigo extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vaadin-vertical-layout theme="spacing" style="width: 100%; height: 100%;">
+ <vaadin-vertical-layout theme="spacing" style="align-items: center; justify-content: center; height: 30%; width: 100%;">
+  <img src="../../resources/icons/toktok2.svg">
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="height: 100%; width: 100%; align-items: center; justify-content: center;">
+  Introduzca el codigo que le hemos enviado
+  <vaadin-text-field id="idCodigo" style="width: 70%;"></vaadin-text-field>
+  <vaadin-button id="idBoton" style="width: 20%;">
+   Enviar 
+  </vaadin-button>
+ </vaadin-vertical-layout>
+</vaadin-vertical-layout>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
--- martinezmellado/frontend/src/vista-establecer_nueva_contrasenna.ts
+++ martinezmellado/frontend/src/vista-establecer_nueva_contrasenna.ts
@@ -1,4 +1,8 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
 @customElement('vista-establecer_nueva_contrasenna')
 export class VistaEstablecer_nueva_contrasenna extends LitElement {
@@ -12,7 +16,21 @@ export class VistaEstablecer_nueva_contrasenna extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vaadin-vertical-layout theme="spacing" style="width: 100%; height: 100%;">
+ <vaadin-horizontal-layout theme="spacing" style="height: 20%; width: 100%; align-items: center; justify-content: center;">
+  <img src="../../resources/icons/toktok2.svg">
+ </vaadin-horizontal-layout>
+ <vaadin-vertical-layout theme="spacing" style="align-items: center; justify-content: center; width: 100%; height: 100%;">
+  Nueva contraseña
+  <vaadin-text-field id="idPass1" style="width: 70%;"></vaadin-text-field>Confirma contraseña 
+  <vaadin-text-field id="idPass2" style="width: 70%;"></vaadin-text-field>
+  <vaadin-button id="idBoton" style="width: 20%;">
+   Aceptar
+  </vaadin-button>
+ </vaadin-vertical-layout>
+</vaadin-vertical-layout>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
