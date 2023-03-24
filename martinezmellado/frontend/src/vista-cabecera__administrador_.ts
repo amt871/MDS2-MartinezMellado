5f007e08b523b9b7b62c8fd85b3b7d5ce490386d
Mellado Garcia Juan Raul (jmg061) <jmg061@inlumine.ual.es>
2023-03-24 01:29:24 +0100

    Fix CabeceraAdmin

--- martinezmellado/frontend/src/vista-cabecera__administrador_.ts
+++ martinezmellado/frontend/src/vista-cabecera__administrador_.ts
@@ -1,8 +1,9 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-icons/vaadin-icons.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
 import '@vaadin/vaadin-button/src/vaadin-button.js';
 import '@polymer/iron-icon/iron-icon.js';
-import '@vaadin/vaadin-icons/vaadin-icons.js';
 
 @customElement('vista-cabecera__administrador_')
 export class VistaCabecera__administrador_ extends LitElement {
@@ -17,25 +18,27 @@ export class VistaCabecera__administrador_ extends LitElement {
 
   render() {
     return html`
-<vaadin-horizontal-layout class="content" style="width: 100%; align-items: flex-start; justify-content: space-between; padding-top: var(--lumo-space-s);">
- <vaadin-button style="margin-left: 5%;">
-   Inicio 
- </vaadin-button>
- <vaadin-button>
-   Busqueda 
- </vaadin-button>
- <vaadin-horizontal-layout theme="spacing" style="margin-right: 5%; flex-grow: 0; flex-shrink: 1;">
-  <vaadin-button style="flex-grow: 0; flex-shrink: 1; align-self: center;">
-    Administrar 
+<vaadin-vertical-layout style="width: 100%; height: 100%; align-items: center; justify-content: center;">
+ <vaadin-horizontal-layout class="content" style="width: 100%; align-items: center; justify-content: space-between;">
+  <vaadin-button style="margin-left: 1%;">
+    Inicio 
   </vaadin-button>
-  <vaadin-button theme="icon" aria-label="Add new" id="iconPerfilButon">
-   <iron-icon icon="lumo:user"></iron-icon>
+  <vaadin-button>
+    Busqueda 
   </vaadin-button>
-  <vaadin-button theme="icon" aria-label="Add new" id="iconExitButton">
-   <iron-icon icon="vaadin:sign-out"></iron-icon>
-  </vaadin-button>
+  <vaadin-horizontal-layout theme="spacing-s" style="margin-right: 1%; flex-grow: 0; flex-shrink: 1;">
+   <vaadin-button style="flex-grow: 0; flex-shrink: 1; align-self: center;">
+     Administrar 
+   </vaadin-button>
+   <vaadin-button theme="icon" aria-label="Add new" id="iconPerfilButon">
+    <iron-icon icon="lumo:user"></iron-icon>
+   </vaadin-button>
+   <vaadin-button theme="icon" aria-label="Add new" id="iconExitButton">
+    <iron-icon icon="vaadin:sign-out"></iron-icon>
+   </vaadin-button>
+  </vaadin-horizontal-layout>
  </vaadin-horizontal-layout>
-</vaadin-horizontal-layout>
+</vaadin-vertical-layout>
 `;
   }
 
