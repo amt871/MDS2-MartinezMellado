6a03f6134f5b737b2ade494e8c2372cb6247bdc5
Alejandro Martiínez Terrones <amt871@inlumine.ual.es>
2023-03-29 20:37:59 +0200

    vista notificaciones

--- martinezmellado/frontend/src/vista-notificaciones.ts
+++ martinezmellado/frontend/src/vista-notificaciones.ts
@@ -1,4 +1,10 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import './vista-mi_cabecera';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-scroller.js';
+import './vista-notificaciones_item';
 
 @customElement('vista-notificaciones')
 export class VistaNotificaciones extends LitElement {
@@ -12,7 +18,34 @@ export class VistaNotificaciones extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vaadin-vertical-layout theme="spacing" style="width: 100%; height: 100%;">
+ <vista-mi_cabecera style="height: 5%; width: 100%;"></vista-mi_cabecera>
+ <vaadin-horizontal-layout theme="spacing" style="width: 100%; align-items: center; justify-content: center;">
+  <vaadin-button id="idComentarios">
+   Comentarios 
+  </vaadin-button>
+  <vaadin-button id="idMeGusta">
+   Me gustas
+  </vaadin-button>
+  <vaadin-button id="idSeguidores">
+   Seguidores 
+  </vaadin-button>
+  <vaadin-button id="idMenciones">
+   Menciones 
+  </vaadin-button>
+  <vaadin-button id="idTodas">
+    Todas
+  </vaadin-button>
+ </vaadin-horizontal-layout>
+ <vaadin-scroller style="width: 100%; height: 100%; padding: var(--lumo-space-l);">
+  <vista-notificaciones_item></vista-notificaciones_item>
+  <vista-notificaciones_item></vista-notificaciones_item>
+  <vista-notificaciones_item></vista-notificaciones_item>
+  <vista-notificaciones_item></vista-notificaciones_item>
+ </vaadin-scroller>
+</vaadin-vertical-layout>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
--- martinezmellado/frontend/src/vista-notificaciones_item.ts
+++ martinezmellado/frontend/src/vista-notificaciones_item.ts
@@ -1,4 +1,8 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@polymer/iron-icon/iron-icon.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
 @customElement('vista-notificaciones_item')
 export class VistaNotificaciones_item extends LitElement {
@@ -12,7 +16,25 @@ export class VistaNotificaciones_item extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vaadin-horizontal-layout theme="spacing" style="width: 100%; height: 100%; align-items: center;">
+ <vaadin-vertical-layout theme="spacing" style="height: 100%; align-self: center; justify-content: center; width: 100%;">
+  <iron-icon id="idFotoUsuario" style="width: 100%; height: 20%;" icon="lumo:user"></iron-icon>
+  <label id="idNombreUsuario" style="align-self: center;">Nombre de usuario</label>
+ </vaadin-vertical-layout>
+ <vaadin-horizontal-layout theme="spacing" style="width: 100%;">
+  <label id="idTextoNotificación" style="align-self: center;">Texto de la notificacion</label>
+ </vaadin-horizontal-layout>
+ <vaadin-vertical-layout theme="spacing" style="width: 100%; align-self: center;">
+  <img id="idVideoRelacionado" style="width: 100%;" src="../../resources/icons/java.svg">
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="width: 100%; align-self: center; align-items: center;">
+  <vaadin-button id="idBotonVista">
+   Vista 
+  </vaadin-button>
+ </vaadin-vertical-layout>
+</vaadin-horizontal-layout>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
