96ebf171f1545879da3149bb229a4861c4cc0d2d
Mellado Garcia Juan Raul (jmg061) <jmg061@inlumine.ual.es>
2023-03-30 11:45:35 +0200

    FixPublicar

--- martinezmellado/frontend/src/vista-confirmar_correo.ts
+++ martinezmellado/frontend/src/vista-confirmar_correo.ts
@@ -1,4 +1,7 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
 @customElement('vista-confirmar_correo')
 export class VistaConfirmar_correo extends LitElement {
@@ -12,7 +15,20 @@ export class VistaConfirmar_correo extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vaadin-vertical-layout style="width: 100%; height: 100%;">
+ <vaadin-vertical-layout style="width: 100%; height: 30%; align-items: center; justify-content: center;">
+  <img src="../../resources/icons/toktok2.svg">
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="width: 100%; height: 70%; align-items: center; justify-content: center;">
+  <label>Introduzca el codigo que le hemos enviado </label>
+  <vaadin-text-field label="" placeholder="" style="width: 70%;"></vaadin-text-field>
+  <vaadin-button style="width: 20%;">
+   Enviar 
+  </vaadin-button>
+ </vaadin-vertical-layout>
+</vaadin-vertical-layout>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
