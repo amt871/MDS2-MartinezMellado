bd971d341cac29c8317d5c4ef8c7b2d71da0487d
Mellado Garcia Juan Raul (jmg061) <jmg061@inlumine.ual.es>
2023-03-31 12:34:43 +0200

    Cosas

--- martinezmellado/frontend/src/vista-me_gusta__no_registrado__item.ts
+++ martinezmellado/frontend/src/vista-me_gusta__no_registrado__item.ts
@@ -1,4 +1,6 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
 
 @customElement('vista-me_gusta__no_registrado__item')
 export class VistaMe_gusta__no_registrado__item extends LitElement {
@@ -12,7 +14,14 @@ export class VistaMe_gusta__no_registrado__item extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vaadin-vertical-layout style="width: 100%; height: 100%; align-items: flex-start;">
+ <vaadin-horizontal-layout theme="" style="width: 100%; height: 100%; align-items: center; justify-content: flex-start;">
+  <img style="height: 90%;" src="../../resources/icons/user.svg">
+  <label style="width: 100%;">Usuario</label>
+ </vaadin-horizontal-layout>
+</vaadin-vertical-layout>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
--- martinezmellado/frontend/src/vista-ver_me_gustas__no_registrado_.ts
+++ martinezmellado/frontend/src/vista-ver_me_gustas__no_registrado_.ts
@@ -1,4 +1,9 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import './vista-cabecera_usuario_no_registrado';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import './vista-me_gusta__no_registrado_';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
 @customElement('vista-ver_me_gustas__no_registrado_')
 export class VistaVer_me_gustas__no_registrado_ extends LitElement {
@@ -12,7 +17,26 @@ export class VistaVer_me_gustas__no_registrado_ extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vaadin-vertical-layout style="width: 100%; height: 100%;">
+ <vista-cabecera_usuario_no_registrado style="width: 100%; height: 5%;"></vista-cabecera_usuario_no_registrado>
+ <vaadin-horizontal-layout style="height: 95%; width: 100%;">
+  <vaadin-vertical-layout style="height: 100%; width: 60%; align-items: flex-start; justify-content: center;">
+   <img style="height: 100%;" src="../../resources/icons/video.svg">
+  </vaadin-vertical-layout>
+  <vaadin-vertical-layout style="height: 100%; width: 40%; justify-content: center; align-items: center;">
+   <img style="height: 20%;" src="../../resources/icons/user.svg">
+   <label>Usuario</label>
+   <vista-me_gusta__no_registrado_ style="width: 90%; height: 40%;"></vista-me_gusta__no_registrado_>
+   <vaadin-horizontal-layout style="width: 90%; justify-content: flex-start; align-items: center;">
+    <vaadin-button style="margin-top: var(--lumo-space-l);">
+     Atras
+    </vaadin-button>
+   </vaadin-horizontal-layout>
+  </vaadin-vertical-layout>
+ </vaadin-horizontal-layout>
+</vaadin-vertical-layout>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
