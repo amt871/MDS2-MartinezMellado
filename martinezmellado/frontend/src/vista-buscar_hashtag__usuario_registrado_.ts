bbff82f8b3c5e0328bcf63edd4d7715678223b60
Alejandro Martiínez Terrones <amt871@inlumine.ual.es>
2023-04-11 18:31:47 +0200

    Update vista-buscar_hashtag__usuario_registrado_.ts

--- martinezmellado/frontend/src/vista-buscar_hashtag__usuario_registrado_.ts
+++ martinezmellado/frontend/src/vista-buscar_hashtag__usuario_registrado_.ts
@@ -1,4 +1,8 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import './vista-mi_cabecera';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import './vista-resultado_busqueda_hastag';
 
 @customElement('vista-buscar_hashtag__usuario_registrado_')
 export class VistaBuscar_hashtag__usuario_registrado_ extends LitElement {
@@ -12,7 +16,17 @@ export class VistaBuscar_hashtag__usuario_registrado_ extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vaadin-vertical-layout theme="spacing" style="width: 100%; height: 100%;">
+ <vista-mi_cabecera style="width: 100%; height: 5%;"></vista-mi_cabecera>
+ <vaadin-horizontal-layout theme="spacing" style="width: 100%; height: 95%;">
+  <vaadin-vertical-layout theme="spacing" style="width: 60%; height: 100%;">
+   <vista-resultado_busqueda_hastag style="width: 100%; height: 100%;"></vista-resultado_busqueda_hastag>
+  </vaadin-vertical-layout>
+  <vaadin-vertical-layout theme="spacing" style="width: 40%; height: 100%;"></vaadin-vertical-layout>
+ </vaadin-horizontal-layout>
+</vaadin-vertical-layout>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
