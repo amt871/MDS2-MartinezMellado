0929381a10252f0c7ef7970bd9199645ae64e1b3
Alejandro Martiínez Terrones <amt871@inlumine.ual.es>
2023-04-12 13:26:14 +0200

    Busqueda de usuario registrado

--- martinezmellado/frontend/src/vista-buscar_usuario__usuario_registrado_.ts
+++ martinezmellado/frontend/src/vista-buscar_usuario__usuario_registrado_.ts
@@ -1,4 +1,10 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import './vista-mi_cabecera';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import './vista-usuario__usuario_registrado_';
+import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
 @customElement('vista-buscar_usuario__usuario_registrado_')
 export class VistaBuscar_usuario__usuario_registrado_ extends LitElement {
@@ -12,7 +18,25 @@ export class VistaBuscar_usuario__usuario_registrado_ extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vaadin-vertical-layout theme="spacing" style="width: 100%; height: 100%;">
+ <vista-mi_cabecera style="width: 100%; height: 5%;"></vista-mi_cabecera>
+ <vaadin-horizontal-layout theme="spacing" style="width: 100%; height: 95%;">
+  <vaadin-vertical-layout theme="spacing" style="width: 60%; height: 100%;">
+   <vista-usuario__usuario_registrado_></vista-usuario__usuario_registrado_>
+  </vaadin-vertical-layout>
+  <vaadin-vertical-layout theme="spacing" style="width: 40%; height: 100%; align-items: center; justify-content: center;">
+   <vaadin-text-field id="TextField" style="width: 60%;"></vaadin-text-field>
+   <vaadin-button id="BuscarUsuario" style="width: 60%;">
+    Buscar Usuario
+   </vaadin-button>
+   <vaadin-button id="BuscarHashtag" style="width: 60%;">
+    Buscar Hashtag 
+   </vaadin-button>
+  </vaadin-vertical-layout>
+ </vaadin-horizontal-layout>
+</vaadin-vertical-layout>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
--- martinezmellado/frontend/src/vista-usuario__usuario_registrado_.ts
+++ martinezmellado/frontend/src/vista-usuario__usuario_registrado_.ts
@@ -1,4 +1,7 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-scroller.js';
+import './vista-usuario__usuario_registrado__item';
 
 @customElement('vista-usuario__usuario_registrado_')
 export class VistaUsuario__usuario_registrado_ extends LitElement {
@@ -12,7 +15,30 @@ export class VistaUsuario__usuario_registrado_ extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vaadin-vertical-layout theme="spacing" style="width: 100%; height: 100%;">
+ <vaadin-scroller style="width: 100%; height: 100%;">
+  <vista-usuario__usuario_registrado__item style="height: 15%; padding-top: var(--lumo-space-m);"></vista-usuario__usuario_registrado__item>
+  <vista-usuario__usuario_registrado__item style="height: 15%; padding-top: var(--lumo-space-m);"></vista-usuario__usuario_registrado__item>
+  <vista-usuario__usuario_registrado__item style="height: 15%; padding-top: var(--lumo-space-m);"></vista-usuario__usuario_registrado__item>
+  <vista-usuario__usuario_registrado__item style="height: 15%; padding-top: var(--lumo-space-m);"></vista-usuario__usuario_registrado__item>
+  <vista-usuario__usuario_registrado__item style="height: 15%; padding-top: var(--lumo-space-m);"></vista-usuario__usuario_registrado__item>
+  <vista-usuario__usuario_registrado__item style="height: 15%; padding-top: var(--lumo-space-m);"></vista-usuario__usuario_registrado__item>
+  <vista-usuario__usuario_registrado__item style="height: 15%; padding-top: var(--lumo-space-m);"></vista-usuario__usuario_registrado__item>
+  <vista-usuario__usuario_registrado__item style="height: 15%; padding-top: var(--lumo-space-m);"></vista-usuario__usuario_registrado__item>
+  <vista-usuario__usuario_registrado__item style="height: 15%; padding-top: var(--lumo-space-m);"></vista-usuario__usuario_registrado__item>
+  <vista-usuario__usuario_registrado__item style="height: 15%; padding-top: var(--lumo-space-m);"></vista-usuario__usuario_registrado__item>
+  <vista-usuario__usuario_registrado__item style="height: 15%; padding-top: var(--lumo-space-m);"></vista-usuario__usuario_registrado__item>
+  <vista-usuario__usuario_registrado__item style="height: 15%; padding-top: var(--lumo-space-m);"></vista-usuario__usuario_registrado__item>
+  <vista-usuario__usuario_registrado__item style="height: 15%; padding-top: var(--lumo-space-m);"></vista-usuario__usuario_registrado__item>
+  <vista-usuario__usuario_registrado__item style="height: 15%; padding-top: var(--lumo-space-m);"></vista-usuario__usuario_registrado__item>
+  <vista-usuario__usuario_registrado__item style="height: 15%; padding-top: var(--lumo-space-m);"></vista-usuario__usuario_registrado__item>
+  <vista-usuario__usuario_registrado__item style="height: 15%; padding-top: var(--lumo-space-m);"></vista-usuario__usuario_registrado__item>
+  <vista-usuario__usuario_registrado__item style="height: 15%; padding-top: var(--lumo-space-m);"></vista-usuario__usuario_registrado__item>
+  <vista-usuario__usuario_registrado__item style="height: 15%; padding-top: var(--lumo-space-m);"></vista-usuario__usuario_registrado__item>
+ </vaadin-scroller>
+</vaadin-vertical-layout>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
