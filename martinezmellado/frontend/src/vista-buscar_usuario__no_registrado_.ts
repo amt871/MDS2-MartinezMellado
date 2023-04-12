13dac9383ad4686f8564c8e920894a591bb90df7
Alejandro Martiínez Terrones <amt871@inlumine.ual.es>
2023-04-12 13:07:22 +0200

    Busqueda ususuarios (no registrado)=

--- martinezmellado/frontend/src/vista-buscar_usuario__no_registrado_.ts
+++ martinezmellado/frontend/src/vista-buscar_usuario__no_registrado_.ts
@@ -1,4 +1,10 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import './vista-cabecera_usuario_no_registrado';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import './vista-usuario__no_registrado_';
+import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
 @customElement('vista-buscar_usuario__no_registrado_')
 export class VistaBuscar_usuario__no_registrado_ extends LitElement {
@@ -12,7 +18,25 @@ export class VistaBuscar_usuario__no_registrado_ extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vaadin-vertical-layout theme="spacing" style="width: 100%; height: 100%;">
+ <vista-cabecera_usuario_no_registrado style="width: 100%; height: 5%;"></vista-cabecera_usuario_no_registrado>
+ <vaadin-horizontal-layout theme="spacing" id="width: 100%; height: 95%;" style="width: 100%; height: 100%;">
+  <vaadin-vertical-layout theme="spacing" style="width: 60%; height: 100%;">
+   <vista-usuario__no_registrado_></vista-usuario__no_registrado_>
+  </vaadin-vertical-layout>
+  <vaadin-vertical-layout theme="spacing" style="width: 40%; height: 100%; align-items: center; justify-content: center;">
+   <vaadin-text-field style="width: 60%;"></vaadin-text-field>
+   <vaadin-button style="width: 60%;">
+     Text Text 
+   </vaadin-button>
+   <vaadin-button style="width: 60%;">
+     Buscar Hashtag 
+   </vaadin-button>
+  </vaadin-vertical-layout>
+ </vaadin-horizontal-layout>
+</vaadin-vertical-layout>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
--- martinezmellado/frontend/src/vista-usuario__no_registrado__item.ts
+++ martinezmellado/frontend/src/vista-usuario__no_registrado__item.ts
@@ -1,4 +1,7 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@polymer/iron-icon/iron-icon.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
 
 @customElement('vista-usuario__no_registrado__item')
 export class VistaUsuario__no_registrado__item extends LitElement {
@@ -12,7 +15,22 @@ export class VistaUsuario__no_registrado__item extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vaadin-horizontal-layout theme="spacing" style="width: 100%; align-items: center;">
+ <iron-icon id="Foto" style="width: 30%; height: 100%;" icon="lumo:user"></iron-icon>
+ <vaadin-vertical-layout theme="spacing" style="align-items: flex-start; justify-content: center; height: 100%;">
+  <label id="NombreUsuario">Usuario </label>
+  <label id="Seguidores">Seguidores </label>
+ </vaadin-vertical-layout>
+ <vaadin-horizontal-layout theme="spacing" style="width: 100%; align-items: center; justify-content: center; align-self: center;">
+  <label id="Descripcion" style="margin-left: var(--lumo-space-xl); flex-grow: 0; flex-shrink: 1;">Descripcion </label>
+ </vaadin-horizontal-layout>
+ <vaadin-vertical-layout style="flex-shrink: 0; align-self: center; align-items: flex-end; margin-top: var(--lumo-space-m); justify-content: center; margin-right: var(--lumo-space-m);" theme="spacing-xs">
+  <img id="video" style="width: 20%;" src="../../resources/icons/video.png">
+  <label style="padding-right: var(--lumo-space-s);">Ultima Publicacion </label>
+ </vaadin-vertical-layout>
+</vaadin-horizontal-layout>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
