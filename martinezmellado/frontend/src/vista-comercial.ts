944d4d8bb409b1bd9fe114d0dce6e396d6401a86
Alejandro Martiínez Terrones <amt871@inlumine.ual.es>
2023-04-12 13:46:11 +0200

    Comercial

--- martinezmellado/frontend/src/vista-comercial.ts
+++ martinezmellado/frontend/src/vista-comercial.ts
@@ -1,4 +1,5 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import './vista-perfil_comercial';
 
 @customElement('vista-comercial')
 export class VistaComercial extends LitElement {
@@ -12,7 +13,9 @@ export class VistaComercial extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vista-perfil_comercial></vista-perfil_comercial>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
