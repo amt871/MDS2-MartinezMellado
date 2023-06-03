15310cb9cd7445fbff986042ddf2caede6d008e9
Alejandro Martiínez Terrones <amt871@inlumine.ual.es>
2023-06-03 19:48:21 +0200

    Avance en subida de video

--- martinezmellado/src/main/java/vistas/VistaPublicar.java
+++ martinezmellado/src/main/java/vistas/VistaPublicar.java
@@ -9,6 +9,15 @@ import com.vaadin.flow.component.template.Id;
 
 import interfaz.Cabecera_comercial;
 import interfaz.Mi_cabecera;
+import com.vaadin.flow.dom.Element;
+
+import basededatos.Usuario_Registrado;
+
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.html.Image;
 
 /**
  * A Designer generated component for the vista-publicar template.
@@ -24,6 +33,18 @@ public class VistaPublicar extends LitTemplate {
 	//private Cabecera_comercial cabeceraCom;
     @Id("divCabecera")
 	private Div divCabecera;
+	@Id("iconoUsr")
+	private Element iconoUsr;
+	@Id("uploader")
+	private Upload uploader;
+	@Id("descripcion")
+	private TextField descripcion;
+	@Id("subirVideo")
+	private Button subirVideo;
+	@Id("prevVideo")
+	private Image prevVideo;
+	
+	private Usuario_Registrado usr;
 
 	/**
      * Creates a new VistaPublicar.
@@ -59,7 +80,54 @@ public class VistaPublicar extends LitTemplate {
 		this.cabecera = cabeceraCom;
 		getDivCabecera().add(cabeceraCom);
 	}
+
+	public Element getIconoUsr() {
+		return iconoUsr;
+	}
+
+	public void setIconoUsr(Element iconoUsr) {
+		this.iconoUsr = iconoUsr;
+	}
+
+	public Upload getUploader() {
+		return uploader;
+	}
+
+	public void setUploader(Upload uploader) {
+		this.uploader = uploader;
+	}
+
+	public TextField getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(TextField descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Button getSubirVideo() {
+		return subirVideo;
+	}
+
+	public void setSubirVideo(Button subirVideo) {
+		this.subirVideo = subirVideo;
+	}
+
+	public Image getPrevVideo() {
+		return prevVideo;
+	}
+
+	public void setPrevVideo(Image prevVideo) {
+		this.prevVideo = prevVideo;
+	}
     
+	public void setUsr(Usuario_Registrado usr) {
+		this.usr = usr;
+	}
+	
+	public Usuario_Registrado getUsr() {
+		return this.usr;
+	}
     
 
 }
