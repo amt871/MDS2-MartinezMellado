a8882cc8f127fb55420ba3f32f4e63856c6196f9
Alejandro Martinez Terrones <amt871@inlumine.ual.es>
2023-06-26 11:41:15 +0200

    Arreglo clases descomentadas

--- martinezmellado/src/main/java/interfaz/Escribir_comentario.java
+++ martinezmellado/src/main/java/interfaz/Escribir_comentario.java
@@ -1,6 +1,8 @@
 package interfaz;
 
-public class Escribir_comentario extends Vista_detalle__usuario_registrado_ {
+import vistas.VistaEscribir_comentario;
+
+public class Escribir_comentario extends VistaEscribir_comentario {
 //	private Button _a�adir_comentario;
 //	private TextArea _textoComentarioTA;
 //	public Vista_detalle__usuario_registrado_ _vista_detalle__usuario_registrado_;
